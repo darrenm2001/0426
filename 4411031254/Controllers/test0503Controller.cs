87d1a39d14c72dea36c0b4d03240dffbc6b2ad3f
darrenmoo <tsc97163@gmail.com>
2023-05-03 20:45:17 +0800

    0503

---
+++ 4411031254/Controllers/test0503Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _4411031254.Controllers
+{
+    public class test0503Controller : Controller
+    {
+        // GET: test0503
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
