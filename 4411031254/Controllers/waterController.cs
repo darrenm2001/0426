9b5af47856911f68cb823c99f3b7c69ae49b569c
darrenmoo <tsc97163@gmail.com>
2023-05-17 19:03:01 +0800

    新增controller喝水

---
+++ 4411031254/Controllers/waterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _4411031254.Controllers
+{
+    public class waterController : Controller
+    {
+        // GET: water
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
