4277e23f2fff8c0fc69e1714d1ce6085cb1b2c17
darrenmoo <tsc97163@gmail.com>
2023-05-17 19:53:44 +0800

    計算喝水建議量 增加header

--- 4411031254/Controllers/waterController.cs
+++ 4411031254/Controllers/waterController.cs
@@ -9,8 +9,17 @@ namespace _4411031254.Controllers
     public class waterController : Controller
     {
         // GET: water
-        public ActionResult Index()
+        public ActionResult Index(string name, float? weight)
         {
+            var result = "";//预设为空
+            if (weight.HasValue)//判断有无输入
+            {
+                result = (weight * 30) + "cc"; //根据体重*30之后加单位
+            }
+            //将数值显示
+            ViewBag.name = name;
+            ViewBag.Result = result;
+
             return View();
         }
     }
