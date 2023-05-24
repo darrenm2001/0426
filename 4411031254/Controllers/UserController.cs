26c94cd389634f075584bfe99f2631068b666333
darren <tsc97163@gmail.com>
2023-05-24 19:38:16 +0800

    建立資料庫models

---
+++ 4411031254/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _4411031254.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(string name, string account, string password)
+        {
+            if (name.IsNullOrWhiteSpace())
+            {
+                ViewBag.NameMessage = "請輸入姓名";
+            }
+            if (account.IsNullOrWhiteSpace())
+            {
+                ViewBag.AccountMessage = "請輸入賬號";
+            }
+            if (password.IsNullOrWhiteSpace())
+            {
+                ViewBag.PasswordMessage = "請輸入密碼";
+            }
+            if (name.IsNullOrWhiteSpace()&& account.IsNullOrWhiteSpace()&& password.IsNullOrWhiteSpace())
+            {
+                ViewBag.NameMessage = "注冊成功";
+            }
+            return View();
+        }
+    }
+}
---
+++ 4411031254/dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Student]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NCHAR(10) NOT NULL, 
+    [Number] NCHAR(10) NOT NULL, 
+    [Email] NCHAR(50) NOT NULL
+)
