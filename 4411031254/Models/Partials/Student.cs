206858a5548fc84d86e0a42585e1d1502cf453c7
darrenmoo <tsc97163@gmail.com>
2023-05-31 19:14:34 +0800

    DataAnnotations

---
+++ 4411031254/Models/Partials/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _4411031254.Models
+{
+    [MetadataType(typeof(StudentMetadata))]
+    
+    public partial class Student
+    {
+        
+    }
+
+    public class StudentMetadata
+    {
+        public int Id{get; set;}
+
+        [Required(ErrorMessage ="請輸入名字")]
+        [Display(Name ="姓名")]
+        [StringLength(12,ErrorMessage = "最多只能輸入10個字")]
+        public string Name{get; set;}
+
+        [Required(ErrorMessage = "請輸入學號")]
+        [Display(Name = "學號")]
+        [StringLength(10, ErrorMessage = "最多只能輸入10個字")]
+        public string Number{get; set;}
+
+        [Required(ErrorMessage = "請輸入電子郵件")]
+        [EmailAddress]
+        public string Email{get; set;}
+    }
+}
